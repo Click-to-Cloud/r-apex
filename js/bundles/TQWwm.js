3fd6171a2ba082a55df69e0f7046a05ddf53b753
Miao (Wilson) Liu <wilson@clicktocloud.com>
2018-05-25 08:27:37 +1000

    update docs

---
+++ js/bundles/TQWwm.js
@@ -0,0 +1,221 @@
+var pageComponent =
+webpackJsonppageComponent([5],{
+
+/***/ 312:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _metalComponent = __webpack_require__(1);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(2);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+__webpack_require__(4);
+
+__webpack_require__(5);
+
+__webpack_require__(6);
+
+__webpack_require__(7);
+
+__webpack_require__(8);
+
+__webpack_require__(9);
+
+__webpack_require__(10);
+
+__webpack_require__(11);
+
+__webpack_require__(12);
+
+__webpack_require__(13);
+
+__webpack_require__(14);
+
+__webpack_require__(15);
+
+var _step_4Soy = __webpack_require__(313);
+
+var _step_4Soy2 = _interopRequireDefault(_step_4Soy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var TQWwm = function (_Component) {
+  _inherits(TQWwm, _Component);
+
+  function TQWwm() {
+    _classCallCheck(this, TQWwm);
+
+    return _possibleConstructorReturn(this, (TQWwm.__proto__ || Object.getPrototypeOf(TQWwm)).apply(this, arguments));
+  }
+
+  return TQWwm;
+}(_metalComponent2.default);
+
+;
+
+_metalSoy2.default.register(TQWwm, _step_4Soy2.default);
+
+exports.default = TQWwm;
+
+/***/ }),
+
+/***/ 313:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.templates = exports.TQWwm = undefined;
+
+var _metalComponent = __webpack_require__(1);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(2);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* jshint ignore:start */
+
+
+var templates;
+goog.loadModule(function (exports) {
+
+  // This file was automatically generated from step_4.soy.
+  // Please don't edit this file by hand.
+
+  /**
+   * @fileoverview Templates in namespace TQWwm.
+   * @public
+   */
+
+  goog.module('TQWwm.incrementaldom');
+
+  /** @suppress {extraRequire} */
+  var soy = goog.require('soy');
+  /** @suppress {extraRequire} */
+  var soydata = goog.require('soydata');
+  /** @suppress {extraRequire} */
+  goog.require('goog.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('soy.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('goog.i18n.bidi');
+  /** @suppress {extraRequire} */
+  goog.require('goog.string');
+  var IncrementalDom = goog.require('incrementaldom');
+  var ie_open = IncrementalDom.elementOpen;
+  var ie_close = IncrementalDom.elementClose;
+  var ie_void = IncrementalDom.elementVoid;
+  var ie_open_start = IncrementalDom.elementOpenStart;
+  var ie_open_end = IncrementalDom.elementOpenEnd;
+  var itext = IncrementalDom.text;
+  var iattr = IncrementalDom.attr;
+
+  var $templateAlias2 = _metalSoy2.default.getTemplate('ElectricCode.incrementaldom', 'render');
+
+  var $templateAlias1 = _metalSoy2.default.getTemplate('tutorial.incrementaldom', 'render');
+
+  /**
+   * @param {{
+   *    page: (?),
+   *    site: (?),
+   *    elementClasses: (?)
+   * }} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $render(opt_data, opt_ignored, opt_ijData) {
+    var param2021 = function param2021() {
+      ie_open('h2');
+      var dyn48 = opt_data.page.title;
+      if (typeof dyn48 == 'function') dyn48();else if (dyn48 != null) itext(dyn48);
+      ie_close('h2');
+      ie_open('p');
+      itext('So far so good. What if we need to handle functions that take variadic arguments? Let\'s extend our ');
+      ie_open('code');
+      itext('AddFunc');
+      ie_close('code');
+      itext(' to allow adding multiple numbers.');
+      ie_close('p');
+      $templateAlias2({ code: 'public class AddFunc extends Func {\n    public AddFunc() {\n        super(-1);\n    }\n\n    public override Object execN(List<Object> args) {\n        Integer sum = 0;\n        for(Object arg : args) {\n            sum += (Integer)arg;\n        }\n        return sum;\n    }\n}', mode: 'javascript' }, null, opt_ijData);
+      ie_open('p');
+      itext('Now this time, we specify the length of the function to be -1, which means that it takes any number of arguments. Also we have our ');
+      ie_open('code');
+      itext('execN(List<Object>)');
+      ie_close('code');
+      itext(' overridden to get the sum of all the numbers.');
+      ie_close('p');
+      ie_open('p');
+      itext('By the way, the default length is -1, so if we want a variadic function, we don\'t even need to call ');
+      ie_open('code');
+      itext('super(-1)');
+      ie_close('code');
+      itext('. In our case, we can simply omit the constructor.');
+      ie_close('p');
+      ie_open('input', null, null, 'type', 'hidden', 'value', opt_data.page.title);
+      ie_close('input');
+      ie_open('input', null, null, 'type', 'hidden', 'value', opt_data.site.title);
+      ie_close('input');
+    };
+    $templateAlias1(soy.$$assignDefaults({ content: param2021 }, opt_data), null, opt_ijData);
+  }
+  exports.render = $render;
+  if (goog.DEBUG) {
+    $render.soyTemplateName = 'TQWwm.render';
+  }
+
+  exports.render.params = ["page", "site"];
+  exports.render.types = { "page": "?", "site": "?" };
+  exports.templates = templates = exports;
+  return exports;
+});
+
+var TQWwm = function (_Component) {
+  _inherits(TQWwm, _Component);
+
+  function TQWwm() {
+    _classCallCheck(this, TQWwm);
+
+    return _possibleConstructorReturn(this, (TQWwm.__proto__ || Object.getPrototypeOf(TQWwm)).apply(this, arguments));
+  }
+
+  return TQWwm;
+}(_metalComponent2.default);
+
+_metalSoy2.default.register(TQWwm, templates);
+exports.TQWwm = TQWwm;
+exports.templates = templates;
+exports.default = templates;
+/* jshint ignore:end */
+
+/***/ })
+
+},[312]);
--- js/bundles/shared.electric.js
+++ js/bundles/shared.electric.js
@@ -5327,8 +5327,8 @@ goog.loadModule(function (exports) {
         ie_void('script', null, null, 'src', '/r-apex/js/bundles/shared.electric.js');
         ie_void('script', null, null, 'src', '/r-apex/vendor/senna/senna.js');
         ie_void('script', null, null, 'src', '/r-apex/vendor/codemirror/lib/codemirror.js');
-        ie_void('script', null, null, 'src', '/r-apex/vendor/codemirror/mode/javascript/javascript.js');
         ie_void('script', null, null, 'src', '/r-apex/vendor/codemirror/mode/css/css.js');
+        ie_void('script', null, null, 'src', '/r-apex/vendor/codemirror/mode/javascript/javascript.js');
         ie_void('script', null, null, 'src', '/r-apex/vendor/codemirror/mode/xml/xml.js');
         ie_open('link', null, null, 'rel', 'stylesheet', 'href', (($$temp = opt_data.site.basePath) == null ? '' : $$temp) + '/styles/main.css');
         ie_close('link');
